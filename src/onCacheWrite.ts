6377eff55f0f3d9a1adfbdfb0db58a79299eef00
Fándly Gergő <gergo@systemtest.tk>
2020-09-18 11:53:36 +0300

    feat: Add trigger for cache.evict and cache.modify, return results of functions

--- src/onCacheWrite.ts
+++ src/onCacheWrite.ts
@@ -1,19 +1,33 @@
-import { ApolloCache } from 'apollo-cache';
-
 export interface TriggerFunctionConfig<T> {
-  cache: ApolloCache<T>;
+  cache: any;
 }
 
 export default <T>({ cache }: TriggerFunctionConfig<T>) => (
-  persist: () => void
+  persist: () => void,
 ) => {
   const write = cache.write;
+  const evict = cache.evict;
+  const modify = cache.modify;
+
   cache.write = (...args: any[]) => {
-    write.apply(cache, args);
+    const result = write.apply(cache, args);
     persist();
+    return result;
+  };
+  cache.evict = (...args: any[]) => {
+    const result = evict.apply(cache, args);
+    persist();
+    return result;
+  };
+  cache.modify = (...args: any[]) => {
+    const result = modify.apply(cache, args);
+    persist();
+    return result;
   };
 
   return () => {
     cache.write = write;
+    cache.evict = evict;
+    cache.modify = modify;
   };
 };
